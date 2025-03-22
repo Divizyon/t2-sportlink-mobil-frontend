6b82b14811d9733764fe4d8199b18cdf97f52c64
ebrartamer <ebrartamer21@gmail.com>
2025-03-22 22:54:14 +0300

    USM-11: Prettier ve ESLint ile kod formatlama ve lint yapılandırması eklendi

---
+++ .eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    'react-native/react-native': true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-native/all',
+    'prettier',
+  ],
+  plugins: ['react', 'react-hooks', 'react-native', '@typescript-eslint', 'prettier'],
+  rules: {
+    // Temel ESLint kuralları
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unused-vars': 'off', // TypeScript kuralı kullanacağız
+
+    // React kuralları
+    'react/prop-types': 'off', // TypeScript kullandığımız için gerekli değil
+    'react/react-in-jsx-scope': 'off', // React 17+ ile gerekli değil
+    'react/display-name': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // React Native kuralları
+    'react-native/no-unused-styles': 'warn',
+    'react-native/split-platform-components': 'warn',
+    'react-native/no-inline-styles': 'warn',
+    'react-native/no-color-literals': 'warn',
+
+    // TypeScript kuralları
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+
+    // Prettier kuralları
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: [
+    'node_modules/',
+    '.expo/',
+    'babel.config.js',
+    'metro.config.js',
+    'jest.config.js',
+  ],
+};
---
+++ progress.md
@@ -0,0 +1,10 @@
+# Proje İlerleme Durumu
+
+## Mobil Frontend
+
+- [x] USM-11 Prettier ve ESLint ile kod formatlama ve lint yapılandırmasının oluşturulması.
+  - [x] ESLint kurulumu ve yapılandırması
+  - [x] Prettier kurulumu ve yapılandırması
+  - [x] VS Code entegrasyonu
+  - [x] Lint ve format komutlarının oluşturulması
+  - [x] README.md güncellenmesi 
